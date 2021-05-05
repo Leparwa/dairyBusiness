734dc8addf0b815a563fa2a457fc6df98815a1f9
leresi <kevinleparwa@gmail.com>
2021-05-05 10:18:11 +0300

    added javascript

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
  
  var totalProduction=[]
+ var totalLitres;
 function shedsProduction() {
     let litres = document.getElementById('litres').value;
-    let production = totalProduction.push(litres)  
-    totalProduction(production)
+    let production = totalProduction.push(litres) 
+    totalLitres = totalProduction.reduce((a, b)=>(a+b), 0) 
   }
 
   function totalProduction(production){
     return production.reduce()
+  }
+  function incomeoverTime(sellingPrice, time){
+    let totalEarning= totalLitres*sellingPrice*time
+    return totalEarning;
   }
