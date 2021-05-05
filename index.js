1e90243b95760196dd08a330982fc6e64cf20a84
leresi <kevinleparwa@gmail.com>
2021-05-05 09:57:41 +0300

    functions

---
+++ index.js
@@ -0,0 +1,11 @@
+ 
+ var totalProduction=[]
+function shedsProduction() {
+    let litres = document.getElementById('litres').value;
+    let production = totalProduction.push(litres)  
+    totalProduction(production)
+  }
+
+  function totalProduction(production){
+    return production.reduce()
+  }
